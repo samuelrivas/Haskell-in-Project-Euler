ec84ce0ef9a94cda0eb108af703fa898d4a645fc
Samuel Rivas <samuel@derek.(none)>
2009-10-12 15:45:10 +0200

    RESTRUCT weed out unused code and nicer layout

--- problem_49.hs
+++ problem_49.hs
@@ -7,27 +7,13 @@ sieve [] = []
 fourDigitPrimes = [x | x <- primes, 1000 <= x]
 
 -- Arithmetic sequences
--- arith_seq (x:xs) = follow x xs : arith_seq xs
--- arith_seq [] = []
-
--- follow _ (y:[]) = []
--- follow _ [] = []
--- follow x (y:ys) = hops x y ys : follow x ys
-
 arith_from x step (y:ys) =
-           if (y - x) `mod` step == 0
-           then
-               y : arith_from x step ys
-           else
-               arith_from x step ys
+    if (y - x) `mod` step == 0
+    then
+        y : arith_from x step ys
+    else
+        arith_from x step ys
 
 arith_from _ _ _ = []
 
--- Digits of a number
--- digits :: Int -> [Char]
-
--- digits n
---     | n < 10 = show n
---     | True = let d = rem n 10
---              in [(show d)]:(digits (div n 10))
 
