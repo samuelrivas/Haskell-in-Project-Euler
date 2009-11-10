3739e63ae4290785671b668557b8ede47df2b6ba
Samuel Rivas <smauelrivas@gmail.com>
2009-11-10 23:48:18 +0100

    FUNCT first approach to a factorisation function

--- problem_3.hs
+++ problem_3.hs
@@ -1,3 +1,16 @@
 -- The prime factors of 13195 are 5, 7, 13 and 29.
 --
 -- What is the largest prime factor of the number 600851475143 ?
+
+-- Infinite list of prime numbers
+primes = sieve [2..]
+sieve (x:xs) = x : sieve [y | y <- xs, mod y x /= 0]
+sieve [] = []
+
+factorisation 1 _ = []
+factorisation n (x:primes) =
+    if n `mod` x == 0
+    then
+        x : (factorisation (n `div` x) (x:primes))
+    else
+        factorisation n primes
